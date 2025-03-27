202c6f11ecffbe7f90126ccbe4355cea53847c38
vjammulapati <vjammulapati@users.noreply.github.com>
2025-03-27 19:32:57 +0000

    Deploy Cairo Urban News Website f7909aac3f8edbccd2693a09caccddab33f6a69d

--- js/index.js
+++ js/index.js
@@ -12,32 +12,20 @@ const index_behaviors = {
       return ref;
     },
 
-    // Add a method to handle adding the data-letter attribute
     "addDataLetter": function() {
-      updateTeiItems(); // Immediately run updateTeiItems when the content is ready
+      const teiItems = document.querySelectorAll('.tei-item[data-origatts="xml:id"]');
+      teiItems.forEach(item => {
+        if (!item.hasAttribute('data-letter')) {
+          const teiName = item.querySelector('tei-name');
+          if (teiName) {
+            const firstLetter = teiName.textContent.trim().charAt(0);
+            item.setAttribute('data-letter', firstLetter);
+          }
+        }
+      });
     }
   }
 };
 
-// Function to update the data-letter for all tei-items with data-origatts="xml:id"
-function updateTeiItems() {
-  const teiItems = document.querySelectorAll('.tei-item[data-origatts="xml:id"]');
-  
-  teiItems.forEach(item => {
-    // Check if the data-letter already exists to prevent unnecessary updates
-    if (!item.hasAttribute('data-letter')) {
-      const teiName = item.querySelector('tei-name');
-      if (teiName) {
-        // Get the first letter of the name
-        const firstLetter = teiName.textContent.trim().charAt(0);
-        // Set the data-letter attribute with the first letter of the name
-        item.setAttribute('data-letter', firstLetter);
-      }
-    }
-  });
-}
-
-// Call the addDataLetter function when the DOM is fully loaded
-document.addEventListener('DOMContentLoaded', function() {
-  index_behaviors["tei"].addDataLetter();
-});
+// Automatically run addDataLetter when the script is executed
+index_behaviors["tei"].addDataLetter();
